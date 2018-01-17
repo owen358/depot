034cad4818e610080a1fce4cf6d45490eea772e7
owen harrison <owenjharrison@protonmail.com>
2018-01-17 13:12:43 +0000

    chapter 8

---
+++ test/controllers/store_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class StoreControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get store_index_url
+    assert_response :success
+    assert_select '#columns #side a', minimum: 4 #looks for an element named a that’s contained in an element with an id with a value of side which is contained within an element with an id with a value of columns.This test verifies that a minimum of four such elements are present.
+    assert_select '#main .entry', 3
+    assert_select 'h3', 'Programming Ruby 1.9'
+    assert_select '.price', /\$[,\d]+\.\d\d/
+  end
+
+end
